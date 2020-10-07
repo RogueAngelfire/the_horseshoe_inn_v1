9544c0443bb15452e6eb29c33b2b62cdb9af7b93
Robin Collins <info@robincollins.co.uk>
2020-10-07 12:20:44 +0000

    Adjusted banner

--- products/migrations/0001_initial.py
+++ products/migrations/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=245)),
-                ('freindly_name', models.CharField(blank=True, max_length=254, null=True)),
+                ('friendly_name', models.CharField(blank=True, max_length=254, null=True)),
             ],
         ),
         migrations.CreateModel(
