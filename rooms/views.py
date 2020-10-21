e319e4ad0deed958317da83563052f3362cdce01
Robin Collins <info@robincollins.co.uk>
2020-10-21 13:16:09 +0000

    Added accomadation links and functionallity

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def rooms(request):
+    """ A view to return the index page """
+
+    return render(request, 'rooms/rooms.html')
