e4bd7f7472f03fc91c57b83ce5347912c0ffa790
Robin Collins <info@robincollins.co.uk>
2020-10-21 15:53:37 +0000

    Added contact and Menu apps with link fuctionallity

--- horseshoe_inn/urls.py
+++ horseshoe_inn/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('rooms/', include('rooms.urls')),
+    path('menu/', include('menu.urls')),
+    path('contact', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- rooms/views.py
+++ rooms/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def rooms(request):
-    """ A view to return the index page """
+    """ A view to return the accomadation page """
 
     return render(request, 'rooms/rooms.html')
