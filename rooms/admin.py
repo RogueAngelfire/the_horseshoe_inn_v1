d3d53b64d310fa1b22e98245a4c250f307bc3ae5
Robin Collins <info@robincollins.co.uk>
2020-10-21 13:16:09 +0000

    Added accomadation links and functionallity

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,4 +2,26 @@ from django.contrib import admin
 from .models import Room
 
 # Register your models here.
+
+""" code didn't work
+class RoomAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'event_dates',
+        'number_available',
+        'is_available',
+        'require_breakfast',
+        'date',
+        'description',
+        'price',
+        'rating',
+        'image_url',
+        'image',
+    )
+
+    booking = ('room_number',)
+    )
+"""
 admin.site.register(Room)
+
+
