4736a0c1a4b47a48ef1f6ede6c2402151ef9eeae
Robin Collins <info@robincollins.co.uk>
2020-11-07 11:38:42 +0000

    Added Rooms functionality

--- menu/views.py
+++ menu/views.py
@@ -3,13 +3,8 @@ from .models import Menu, Category
 
 # Create your views here.
 
-def menu_items(request):
-    """ A view to show all Menu items, including sorting - No search required at this stage of development """
-
 def menu(request):
     """ A view to return the menu page """
-
-    categories = Category.objects.all()
 
     menu = Menu.objects.all()
 
