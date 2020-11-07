67c3558a70693386126c1872fd6822444f360d8b
Robin Collins <info@robincollins.co.uk>
2020-11-07 15:22:43 +0000

    updated links for view get requests

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Room, Category
 
 # Create your views here.
@@ -13,3 +13,14 @@ def rooms(request):
     }
 
     return render(request, 'rooms/rooms.html', context)
+
+def room_detail(request, room):
+    """ A view to show individual room details """
+
+    room = get_object_or_404(Room, pk=room_id)
+
+    context = {
+        'room': room,
+    }
+
+    return render(request, 'rooms/room_detail.html', context)
