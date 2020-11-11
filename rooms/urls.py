88e712c397ad422741d94e24b6da7faf8fc51847
Robin Collins <info@robincollins.co.uk>
2020-11-11 13:27:10 +0000

    Fixed function with missing / on urls for Rooms

--- rooms/urls.py
+++ rooms/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.rooms, name='rooms'),
-    path('<room_id>', views.room_detail, name='room_detail'),
+    path('<room_id>/', views.room_detail, name='room_detail'),
 ]
