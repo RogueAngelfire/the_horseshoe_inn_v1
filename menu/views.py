0266982a404c2920702cfcccb31ce75a9fddbd6f
Robin Collins <info@robincollins.co.uk>
2020-11-11 17:01:10 +0000

    Fixed accomadation and got menu function working

--- menu/views.py
+++ menu/views.py
@@ -7,6 +7,16 @@ def menu(request):
     """ A view to return the menu page """
 
     menu = Menu.objects.all()
+    category = None
+    if request.GET:
+        if 'category' in request.GET:
+            print(f"categories in the request: {request.GET['category']}")
+            categories = request.GET['category'].split(',')
+            print(f"categories are: {categories}")
+            menu = menu.filter(category__name__in=categories)
+            print(f"menu are: {menu}")
+            category = Category.objects.filter(name__in=categories)
+
 
     context = {
         'menu': menu,
