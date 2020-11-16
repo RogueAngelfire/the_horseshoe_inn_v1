fbbb122004ff4e7c208ec37b8dc1140b87a89c79
Robin Collins <info@robincollins.co.uk>
2020-11-16 12:30:36 +0000

    Added bag context

--- horseshoe_inn/settings.py
+++ horseshoe_inn/settings.py
@@ -79,6 +79,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # Required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -158,3 +159,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# For the increase in price for additional people per room
+FREE_DELIVERY_TRESHOLD = 10
