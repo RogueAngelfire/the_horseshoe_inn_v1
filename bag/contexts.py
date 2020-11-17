098e8fbcc3687474f37562c967480282594c3586
Robin Collins <info@robincollins.co.uk>
2020-11-17 15:06:45 +0000

    Updated context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,19 +1,32 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from rooms.models import Room
 
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
-    product_count = 0
+    room_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        room = get_object_or_404(Room, pk=item_id)
+        total += quantity * room.price
+        room_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'room': room,
+        })
 
     grand_total = total
 
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count,
+        'room_count': room_count,
         'grand_total': grand_total,
     }
 
