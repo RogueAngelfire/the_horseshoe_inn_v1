555414bdc1311272db3643890a4e2edfed2213e1
Robin Collins <info@robincollins.co.uk>
2020-11-18 17:05:35 +0000

    Altered booking views

--- book/views.py
+++ book/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.shortcuts import get_object_or_404
+from django.contrib import messages
 from rooms.models import Room
 
 # Create your views here.
@@ -9,12 +10,52 @@ def view_book(request):
 
     return render(request, 'book/book.html')
 
+
 def add_to_book(request, item_id):
     """ Add a quantity of the specified rooms to the booking shopping bag """
 
+    room = get_object_or_404(Room, pk=item_id)
     quantity = int(request.POST.get('quantity'))
+    checkin_date = request.POST.get('checkin_date')
+    checkout_date = request.POST.get('checkout_date')
+    number_of_nights = int(request.POST.get('number_of_nights'))
     redirect_url = request.POST.get('redirect_url')
     book = request.session.get('book', {})
+
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
+    else:
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
+
+    if 'checkin_date' in request.POST:
+        date = request.POST['checkin_date']
+        book = request.session.get('book', {})
+    if date:
+        if item_id in list(book.keys()):
+            if date in book[item_id] ['items_by_date'].keys():
+                book[item_id]['items_by_date'][date]['number_guests'] += quantity
+                book[item_id]['items_by_date'][date]['number_of_nights'] += number_of_nights
+                messages.success(request, f'booking for arrival on {date.upper()} for {room.name} is confirmed')
+            else:
+                book[item_id]['items_by_date'][date] ={}
+                book[item_id]['items_by_date'][date]['number_guests'] = quantity
+                book[item_id]['item_by_date'][date]['number_nights'] = number_of_nights
+                messages.success(request, f'Date added for {date.upper()} {room.name} to your booking')
+        else:
+            book[item_id] = {'items_by_date': {date: {'number_guests': quantity, 'number_of_nights': number_of_nights}}}
+            messages.success(request, f'Added a date {date.upper()} {room.name} to your booking')
 
     if item_id in list(book.keys()):
         book[item_id] += quantity
@@ -23,3 +64,33 @@ def add_to_book(request, item_id):
 
     request.session['book'] = book
     return redirect(redirect_url)
+
+def adjust_booking(request, item_id):
+    """ Adjust the guest quantity """
+
+    room = get_object_or_404(Room, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'room_size' in request.POST:
+        size = request.POST['room_size']
+    book = request.session.get('book', {})
+
+    if size:
+        if quantity > 0:
+            book[item_id]['items_by_size'][size] = quantity
+            messages.success(request, f'Updated room {size.upper()}{room.name} quantity to {book[item_id]["items_by_size"][size]}')
+        else:
+            del book[item_id]['items_by_size'][size]
+            if not book[item_id]['items_by_size']:
+                book.pop(item_id)
+                messages.success(request, f'Removed Room {size.upper()} {room.name} from your booking')
+    else:
+        if quantity > 0:
+            book[item_id] = quantity
+            messages.success(request, f'Removed Room {room.name} from your booking')
+        else:
+            book.pop(item_id)
+            messages.success(request, f'Removes {room.name} from your booking')
+
+    request.session['book'] = book
+    return redirect(reverse('view_book'))
