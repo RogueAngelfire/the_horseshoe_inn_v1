448e7a04cd42b534f5cd510385d49c5e31a1c6c6
Robin Collins <info@robincollins.co.uk>
2020-11-21 12:50:06 +0000

    Added checkout views and templates

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    book = request.session.get('book', {})
+    if not book:
+        messages.error(request, "You have no bookings at the moment")
+        return redirect(reverse('rooms'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
+
