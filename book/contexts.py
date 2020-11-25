7760f7ad794e6b23c014bb49b4135030f14f5876
Robin Collins <info@robincollins.co.uk>
2020-11-25 20:29:43 +0000

    Got function back to book app views and contexts

--- book/contexts.py
+++ book/contexts.py
@@ -2,7 +2,6 @@ from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from rooms.models import Room
-import datetime
 
 
 def book_contents(request):
@@ -11,18 +10,19 @@ def book_contents(request):
     total = 0
     room_count = 0
     book = request.session.get('book', {})
-    print(book)
+    print(f"book is {book}")
     print(f"BOOK ITEMS: {book.items()}")
-    
+
     for item_id, booking_detail in book.items():
         room = get_object_or_404(Room, pk=item_id)
-        item_by_date = booking_detail['item_by_date']
+        items_by_date = booking_detail['items_by_date']
         number_of_nights = booking_detail['number_of_nights']
         number_guests = booking_detail['number_guests']
         total_room_price = number_of_nights * room.price
         book_items.append({
             'item_id': item_id,
             'items_by_date': items_by_date,
+            'number_of_nights': number_of_nights,
             'number_guests': number_guests,
         })
 
@@ -30,8 +30,7 @@ def book_contents(request):
 
     context = {
         'book_items': book_items,
-        'total_room_price': total_room_price,
-        'room_count': room_count,
+         # 'total_room_price': total_room_price,
         'grand_total': grand_total,
     }
 
