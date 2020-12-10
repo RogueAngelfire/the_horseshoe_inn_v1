69c878ee08ff3219b4e9d0733719666a4084aac5
Robin Collins <info@robincollins.co.uk>
2020-12-10 14:42:34 +0000

    Removed secret key and set debug

--- horseshoe_inn/settings.py
+++ horseshoe_inn/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q*g14jd@81l%9rg02e=j8sn_*_pjg&udefz2ynnq8fd4e1-=h#'
-
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-horseshoe-inn.herokuapp.com', 'localhost']
 
